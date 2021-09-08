2db4eb28e5b775d7882cd226295c866af17afaa9
GaurSanidhya <89745472+GaurSanidhya@users.noreply.github.com>
2021-09-08 23:19:49 +0530

    Add files via upload

---
+++ SarLogAnalysisS/src/com/sani/sla/cpu/SarCpuAggregator.java
@@ -0,0 +1,41 @@
+package com.sani.sla.cpu;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class SarCpuAggregator
+{
+	public static void main(String[] args) throws Exception
+	{
+		Configuration conf = new Configuration();
+		/*String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 2)
+		{
+			System.err.println("Usage: Process Sar Logs <in> <out>");
+			System.exit(2);
+		}*/
+		Job job = new Job(conf, "Process Sar Logs");
+
+		job.setJarByClass(SarCpuAggregator.class);
+		job.setMapperClass(SarCpuMappper.class);
+		job.setReducerClass(SarCpuReducer.class);
+		
+		job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(FloatWritable.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		
+		FileInputFormat.addInputPath(job, new Path("/home/sanidhya/Data/cpu_sample_logs_02.txt"));
+		FileOutputFormat.setOutputPath(job, new Path("/home/sanidhya/output/sar-logs-out-04"));
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
